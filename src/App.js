64805914aa0b75bc0317065e3ea8bc4b70251495
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2023-04-02 23:41:37 +0530

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "../src/dist/styles.css";
+import "../src/dict/styles.css";
 import About from "./Pages/About";
 import Home from "./Pages/Home";
 import Navbar from "../src/components/Navbar";
